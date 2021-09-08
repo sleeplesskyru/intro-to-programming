5f96fd1b8bf4e5a87710ee0d8cfc9035e308915d
Kyle <39460375+xkyrux@users.noreply.github.com>
2021-09-07 20:40:24 -0700

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Please say something...\n");
+float radius, area;
+char str[25];
+
+int main(void) {
+    bool restart = false;
+    do {
+        restart = false;
+        system("cls");
+
+        printf("Enter the radius: \n");
+
+        if (scanf("%f", &radius) != 1) {
+            printf("The radius must be a number.\n\n");
+            
+            system("pause");
+
+            restart = true;
+        };
+
+        printf("You said %.1f... Is that right?\n", radius);
+        scanf("%s", str);
+
+        if (strcmp(strlwr(str), "yes") == 0 || strcmp(strlwr(str), "y") == 0) {
+            area = (float) (3.14159 * radius * radius);
+
+            printf("The area of the circle is: %.1f\n\n", area);
+
+            system("pause");
+        } else if (strcmp(strlwr(str), "no") == 0 || strcmp(strlwr(str), "n") == 0) {
+            restart = true;
+        };
+    } while(restart);
+
+    return 0;
 };
